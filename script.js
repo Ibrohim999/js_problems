c9e24f727bc51aae3c26c65fe82d8fae88b87067
Ibohim <Ibohim>
2024-03-09 15:49:23 +0500

    git qoshdim

---
+++ script.js
@@ -0,0 +1,40 @@
+var name = prompt(" Ismingizni kiriting")
+    console.log("ismim " + name);
+var age = prompt(" yoshingizni kiriting")
+    console.log("yoshim " + age + "yoshda")
+var misol1 = prompt("birinchi misol: 9 + 9 = ?")
+if (misol1==18){
+    console.log("javobingiz tog'ri " + misol1)
+}
+else{
+    console.log("javobingiz xato " + misol1)
+}
+var misol2 = prompt("ikkinchi misol: 9 - 8 = ?")
+if (misol2==1){
+    console.log("javobingiz tog'ri " + misol2)
+}
+else{
+    console.log("javobingiz xato " + misol2)
+}
+var misol3 = prompt("uchinchi misol: 9 * 9 = ?")
+if (misol3==81){
+    console.log("javobingiz tog'ri " + misol3)
+}
+else{
+    console.log("javobingiz xato " + misol3)
+}
+var misol4 = prompt("to'rtinchi misol: 9 / 9 = ?")
+if (misol4==1){
+    console.log("javobingiz tog'ri " + misol4)
+}
+else{
+    console.log("javobingiz xato " + misol4)
+}
+var misol5 = prompt("beshinchi misol: 9 / 2 = ?")
+if (misol5==4.5){
+    console.log("javobingiz tog'ri " + misol5)
+}
+else{
+    console.log("javobingiz xato " + misol5)
+}
+alert("natijani ko'rish uchun consolega o'ting")
